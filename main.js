166c6ce201a493d9b52722a0ba788705d95d5042
Ezy93 <giordani001@gmail.com>
2022-03-14 15:31:20 +0100

    generati dinamicamente i post

--- main.js
+++ main.js
@@ -57,7 +57,7 @@ const posts = [
     {
         "id": 6,
         "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
-        "media": "https://unsplash.it/600/400?image=24",
+        "media": "https://unsplash.it/600/400?image=34",
         "author": {
             "name": "Doriana Estebanera",
             "image": null
@@ -68,10 +68,10 @@ const posts = [
     {
         "id": 7,
         "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
-        "media": "https://unsplash.it/600/400?image=24",
+        "media": "https://unsplash.it/600/400?image=44",
         "author": {
             "name": "Luca Ciuffreda",
-            "image": "https://unsplash.it/300/300?image=29",
+            "image": "https://unsplash.it/300/300?image=39",
         },
         "likes": 69,
         "created": "2021-04-01"
@@ -79,7 +79,7 @@ const posts = [
     {
         "id": 8,
         "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
-        "media": "https://unsplash.it/600/400?image=24",
+        "media": "https://unsplash.it/600/400?image=54",
         "author": {
             "name": "Francesco Giordani",
             "image": null
@@ -90,10 +90,10 @@ const posts = [
     {
         "id":9,
         "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
-        "media": "https://unsplash.it/600/400?image=24",
+        "media": "https://unsplash.it/600/400?image=64",
         "author": {
             "name": "Giovanni paolo II",
-            "image": "https://unsplash.it/300/300?image=29",
+            "image": "https://unsplash.it/300/300?image=49",
         },
         "likes": 56,
         "created": "2021-01-01"
@@ -101,10 +101,10 @@ const posts = [
     {
         "id": 10,
         "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
-        "media": "https://unsplash.it/600/400?image=24",
+        "media": "https://unsplash.it/600/400?image=74",
         "author": {
             "name": "Roland crescimone",
-            "image": "https://unsplash.it/300/300?image=29",
+            "image": "https://unsplash.it/300/300?image=59",
         },
         "likes": 87,
         "created": "2021-04-03"
@@ -112,10 +112,10 @@ const posts = [
     {
         "id": 11,
         "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
-        "media": "https://unsplash.it/600/400?image=24",
+        "media": "https://unsplash.it/600/400?image=84",
         "author": {
             "name": "Luca Farinola",
-            "image": "https://unsplash.it/300/300?image=29",
+            "image": "https://unsplash.it/300/300?image=69",
         },
         "likes": 102,
         "created": "2021-01-01"
@@ -123,10 +123,10 @@ const posts = [
     {
         "id": 12,
         "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
-        "media": "https://unsplash.it/600/400?image=24",
+        "media": "https://unsplash.it/600/400?image=94",
         "author": {
             "name": "Bertuccia Cappuccino",
-            "image": "https://unsplash.it/300/300?image=29",
+            "image": "https://unsplash.it/300/300?image=79",
         },
         "likes": 150,
         "created": "2020-04-03"
@@ -134,10 +134,10 @@ const posts = [
     {
         "id": 13,
         "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
-        "media": "https://unsplash.it/600/400?image=24",
+        "media": "https://unsplash.it/600/400?image=104",
         "author": {
             "name": "Mariagrazia Troiano",
-            "image": "https://unsplash.it/300/300?image=29",
+            "image": "https://unsplash.it/300/300?image=89",
         },
         "likes": 89,
         "created": "2021-12-03"
@@ -145,10 +145,10 @@ const posts = [
     {
         "id": 14,
         "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
-        "media": "https://unsplash.it/600/400?image=24",
+        "media": "https://unsplash.it/600/400?image=14",
         "author": {
             "name": "Giovannino Di quarto",
-            "image": "https://unsplash.it/300/300?image=29"
+            "image": "https://unsplash.it/300/300?image=99"
         },
         "likes": 117,
         "created": "2022-04-03"
@@ -156,10 +156,10 @@ const posts = [
     {
         "id": 15,
         "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
-        "media": "https://unsplash.it/600/400?image=24",
+        "media": "https://unsplash.it/600/400?image=114",
         "author": {
             "name": "Esteban De la suerte",
-            "image": "https://unsplash.it/300/300?image=29",
+            "image": "https://unsplash.it/300/300?image=109",
         },
         "likes": 502,
         "created": "2021-04-03"
@@ -179,3 +179,39 @@ Milestone 2 - Prendendo come riferimento il layout di esempio presente nell'html
 
 Milestone 3 - Se clicchiamo sul tasto "Mi Piace" cambiamo il colore al testo del bottone e incrementiamo il counter dei likes relativo. 
             Salviamo in un secondo array gli id dei post ai quali abbiamo messo il like. */
+
+const postContainer = document.getElementById("container");
+
+posts.forEach(element => {
+    postContainer.innerHTML += `
+    <div class="post">
+            <div class="post__header">
+                <div class="post-meta">                    
+                    <div class="post-meta__icon">
+                        <img class="profile-pic" src="${element.author.image}" alt="${element.author.name}">                    
+                    </div>
+                    <div class="post-meta__data">
+                        <div class="post-meta__author">${element.author.name}</div>
+                        <div class="post-meta__time">${element.created}</div>
+                    </div>                    
+                </div>
+            </div>
+            <div class="post__text">${element.content}</div>
+            <div class="post__image">
+                <img src=${element.media} alt="post image">
+            </div>
+            <div class="post__footer">
+                <div class="likes js-likes">
+                    <div class="likes__cta">
+                        <a class="like-button  js-like-button" href="#" data-postid="${element.id}">
+                            <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                            <span class="like-button__label">Mi Piace</span>
+                        </a>
+                    </div>
+                    <div class="likes__counter">
+                        Piace a <b id="like-counter-1" class="js-likes-counter">${element.likes}</b> persone
+                    </div>
+                </div> 
+            </div>
+    `
+});
